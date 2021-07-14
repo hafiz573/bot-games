5e5f366f0d01e9a892add0f6db2860ac7d128d38
HZR_H3RY <79120962+hafiz573@users.noreply.github.com>
2021-07-14 23:43:27 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js')
+const DiscordJS = require('discord.js')
+const WOKCommands = require('wokcommands')
+
+const testServer = '820906598783516702'
+const ownerID = '796251215968075796'
+
+const client = new DiscordJS.Client({
+    partials: ['MESSAGE', 'REACTION']
+})
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} is online`) 
+  client.user.setPresence({
+    activity: {
+      type: "PLAYING",
+      name: "JP store game bot"
+    },
+    status: "idle"
+  });
+
+require("http").createServer((req, res) => res.end("alive")).listen();
+
+    new WOKCommands(client, {
+    commandsDir: 'commands',
+    featuresDir: 'features',
+    messagesPath: 'messages.json',
+    testServers: [testServer]
+  })
+    .setDefaultPrefix(process.env.PREFIX)
+    .setColor('BLUE')
+    .setMongoPath(process.env.MONGO_URI)
+    .setBotOwner([ownerID])
+    .setCategorySettings([
+      {
+        name: 'Games',
+        emoji: '🎮',
+      },
+      {
+        name: 'Development',
+        emoji: '⚙',
+        hidden: true
+      },
+      {
+        // You can change the default emojis as well
+        // "Configuration" is ⚙ by default
+        name: 'Configuration',
+        emoji: '🚧',
+        // You can also hide a category from the help menu
+        // Admins bypass this
+        hidden: true
+      }
+  ])
+})
+
+client.login(process.env.TOKEN)
